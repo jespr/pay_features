f2eea3ec6c8aa15896069e11357630592477eee1
Jesper Christiansen <hi@jespr.com>
2019-12-31 10:01:55 -0800

    Test + cleanup

--- test/pay_features_test.rb
+++ test/pay_features_test.rb
@@ -6,4 +6,12 @@ class PayFeatures::Test < ActiveSupport::TestCase
   test 'truth' do
     assert_kind_of Module, PayFeatures
   end
+
+  test "default plan class is Plan" do
+    assert_equal 'Plan', PayFeatures.plan_class
+  end
+
+  test "default plan table is plans" do
+    assert_equal "plans", PayFeatures.plan_table
+  end
 end
